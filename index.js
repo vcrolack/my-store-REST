057da3baf0046a80a1c785df6586ceb9e1beaf59
Victor Rolack <victorolack@gmail.com>
2022-11-26 17:38:25 -0300

    testing express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My app');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("testing app");
+});
+
+app.listen(port, () => {
+  console.log("I'm running on port " + port);
+})
