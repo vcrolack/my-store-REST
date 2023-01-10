dc6090cc7877eb4e9de933592244c9d555c87401
Victor Rolack <victorolack@gmail.com>
2023-01-10 20:34:07 -0300

    add ssl

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const { configPostgres } = require('../config/config');
 
 const USER = encodeURIComponent(configPostgres.dbUser);
 const PASSWORD = encodeURIComponent(configPostgres.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${configPostgres.dbHost}:${configPostgres.dbPort}/${configPostgres.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${configPostgres.dbHost}:${configPostgres.dbPort}/${configPostgres.dbName}?sslmode=require`;
 
 module.exports = {
   development: {
