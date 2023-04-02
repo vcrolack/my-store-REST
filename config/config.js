dcce03dbf175c3bdcdbc058665b12643065a13bc
Victor Rolack <victorolack@gmail.com>
2023-04-02 18:51:28 -0400

    trying bcrypt

--- config/config.js
+++ config/config.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 
+const config = {
+  apiKey: process.env.API_KEY,
+};
+
 const configPostgres = {
   env: process.env.NODE_ENV || 'dev',
   dbPort: process.env.DB_PORT || 3000,
   dbUser: process.env.POSTGRES_USER,
   dbPassword: process.env.POSTGRES_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.POSTGRES_DB
-}
+  dbName: process.env.POSTGRES_DB,
+};
 
 const configMySQL = {
   env: process.env.NODE_ENV || 'dev',
@@ -15,7 +19,7 @@ const configMySQL = {
   dbUser: process.env.MYSQL_USER,
   dbPassword: process.env.MYSQL_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.MYSQL_DB
-}
+  dbName: process.env.MYSQL_DB,
+};
 
-module.exports = { configPostgres, configMySQL };
+module.exports = { configPostgres, configMySQL, config };
